7251935a56e0666ef6be03e164ea636152b1dff0
unknown <3590127324@qq.com>
2018-05-16 15:43:56 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<style scoped>
+
+</style>
+<template>
+    <div class="index">
+        123
+    </div>
+</template>
+<script>
+
+    export default {
+        components: {},
+        methods: {}
+
+    };
+</script>
